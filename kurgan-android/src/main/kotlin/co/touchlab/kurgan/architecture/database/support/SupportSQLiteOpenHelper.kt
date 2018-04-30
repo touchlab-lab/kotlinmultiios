9ff2822c7ab92f1b40f5aa3924866c0f83972e73
Kevin Galligan <kgalligan@gmail.com>
2018-04-29 22:01:05 -0400

    Copied to android

--- kurgan-android/src/main/kotlin/co/touchlab/kurgan/architecture/database/support/SupportSQLiteOpenHelper.kt
+++
@@ -1,8 +0,0 @@
-package co.touchlab.kurgan.architecture.database.support
-
-import android.database.sqlite.SQLiteDatabase
-import java.io.File
-
-actual fun deleteDatabase(path:String):Boolean{
-    return SQLiteDatabase.deleteDatabase(File(path))
-}
